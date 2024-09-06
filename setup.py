995e6982f1a2e9f55b7736e8198505a754318656
Borohydride <3171132517@qq.com>
2024-09-06 09:05:45 +0800

    None

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="2.0",
     author="Borohydride",
     author_email="3171132517@qq.com",
-    description="A package for calculating the interfacial energy of a system",
+    description="A tool for handling iFAction resource file (iFCon file)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/iFCon/iFCon",
